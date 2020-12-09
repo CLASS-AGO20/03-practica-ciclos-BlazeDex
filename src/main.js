160622e3bb945c690f84ce758853ab4561940ee3
Alan Hernández <ahernandez102@ucol.mx>
2020-12-09 16:10:24 -0600

    Implemented factorial()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
-export default class App {}
+export default class App {
+    factorial(numero) {        
+        let r = 1;
+        for(let i = numero; i > 0; i--) {
+            r = r * i;
+        }
+        return r;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
 let app = new App();
+console.log("Probando: factorial()");
+console.log(app.factorial(4));
+
